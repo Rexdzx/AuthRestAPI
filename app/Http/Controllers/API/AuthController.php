3819411263059ec129e3972b4c523739f5aa14c1
Muhammad Rafli <raflylagitobat09@gmail.com>
2023-02-01 11:42:41 +0700

    Create AuthController

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,75 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenInvalidException;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Register Success',
+            'data' => $user,
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+
+        // validator
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response($validator->errors());
+        }
+        // ambil data user
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        if (!$token = Auth::guard('api')->attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Email atau Password Salah'
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'Berhasil Login',
+            'data' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function logout()
+    {
+        JWTAuth::invalidate(JWTAuth::getToken());
+
+        return response()->json([
+            'message' => 'Berhasil Logout'
+        ], 200);
+    }
 }
