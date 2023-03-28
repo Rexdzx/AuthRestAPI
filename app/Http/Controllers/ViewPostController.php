17ac2714700dc5cbe50dd34be3ff8c44c2b23ec9
Muhammad Rafli <raflylagitobat09@gmail.com>
2023-03-29 00:28:20 +0700

    update ui controller

--- app/Http/Controllers/ViewPostController.php
+++ app/Http/Controllers/ViewPostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use PDF;
 
 class ViewPostController extends Controller
 {
